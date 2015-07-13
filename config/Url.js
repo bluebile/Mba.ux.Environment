6a0af23d3b02e4b5c050ce6e39cf61df5508b152
ramonornela <ramon.ornela@gmail.com>
2015-07-13 19:00:23 -0300

    [UPDATE]
    - Implementacao url env para ajax

---
+++ config/Url.js
@@ -0,0 +1,39 @@
+Ext.define('Mba.ux.Environment.config.Url', {
+    singleton: true,
+    mixins: [
+        'Mba.ux.BuilderConfig.mixin.BuilderConfig'
+    ],
+    alternateClassName: 'Url',
+
+    constructor: function()
+    {
+        var env = Mba.ux.Environment.get();
+        if (env === null) {
+            throw 'E requerido atribuir o ambiente';
+        }
+    },
+
+    set: function(id, value)
+    {
+        var object= {}, merge;
+        object[id] = value
+        merge = Ext.apply(this.getAll(), object);
+
+        return Mba.ux.BuilderConfig.set('url', merge, true);
+    },
+
+    has: function(id)
+    {
+        return Ext.isObject(Mba.ux.BuilderConfig.get('url')) && id in Mba.ux.BuilderConfig.get('url');
+    },
+
+    get: function(id)
+    {
+        return Mba.ux.BuilderConfig.get('url')[id];
+    },
+
+    getAll: function()
+    {
+        return Mba.ux.BuilderConfig.get('url');
+    }
+});
--- overrides/Connection.js
+++ overrides/Connection.js
@@ -1,3 +1,18 @@
 Ext.define('Mba.ux.Environment.overrides.Connection', {
-    override: 'Ext.data.Connection'
+    override: 'Ext.data.Connection',
+    requires: [ 'Mba.ux.Environment.config.Url' ],
+
+    setupUrl: function(options, url)
+    {
+        var form = this.getForm(options);
+        if (form) {
+            url = url || form.action;
+        }
+
+        if (!Mba.ux.Environment.config.Url.has(url)) {
+            return url;
+        }
+
+        return Mba.ux.Environment.config.Url.get(url);
+    }
 });
