8ab08d51f6b0402537eedabb50b4184e1150b17d
ramonornela <ramon.ornela@gmail.com>
2015-09-02 14:46:09 -0300

    [UPDATE]
    - Correcao url crua sem configuracao definida

--- overrides/Server.js
+++ overrides/Server.js
@@ -2,24 +2,12 @@ Ext.define('Mba.ux.Environment.overrides.ServerProxy', {
     override: 'Ext.data.proxy.Server',
     requires: [ 'Mba.ux.Environment.config.Url' ],
 
-    buildUrl: function(request)
-    {
-        var url = request.getUrl();
-        if (Url.has(url)) {
-            return this.callParent([request]);
-        }
-
-        if (this.getNoCache()) {
-            url = Ext.urlAppend(url, Ext.String.format('{0}={1}', this.getCacheString(), Ext.Date.now()));
-        }
-
-        return url;
-    },
-
     getUrl: function(request)
     {
-        return request ?
-            Url.get(request.getUrl() || this.getApi()[request.getAction()] || this._url) :
-            Url.get(this._url);
+        var url = request ?
+            request.getUrl() || this.getApi()[request.getAction()] || this._url :
+            this._url;
+
+        return Url.has(url) ? Url.get(url) : url;
     }
 });
