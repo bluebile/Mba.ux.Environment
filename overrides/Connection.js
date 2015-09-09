695d48920f620850bd8e64bac2388193aa68fc55
ramonornela <ramon.ornela@gmail.com>
2015-09-09 15:46:48 -0300

    [UPDATE]
    - Melhoria parametros como URL suporte para servicos do RNC

--- overrides/Connection.js
+++ overrides/Connection.js
@@ -4,7 +4,8 @@ Ext.define('Mba.ux.Environment.overrides.Connection', {
 
     setupUrl: function(options, url)
     {
-        var form = this.getForm(options);
+        var form = this.getForm(options),
+            regex = null;
         if (form) {
             url = url || form.action;
         }
@@ -13,6 +14,14 @@ Ext.define('Mba.ux.Environment.overrides.Connection', {
             return url;
         }
 
-        return Mba.ux.Environment.config.Url.get(url);
+        url = Mba.ux.Environment.config.Url.get(url);
+
+        if (options.paramsUrl) {
+            for (var i in paramsUrl) {
+                regex = new RegExp('\{' + i + '\}', i);
+                url = url.replace(regex, paramsUrl[i]);
+            }
+        }
+        return url;
     }
 });
