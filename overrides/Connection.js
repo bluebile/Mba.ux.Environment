509f87f90d8af69bcfdbf674d0765df01c050318
ramonornela <ramon.ornela@gmail.com>
2015-09-09 16:39:47 -0300

    [UPDATE]
    - Ajuste token servico

--- overrides/Connection.js
+++ overrides/Connection.js
@@ -4,24 +4,30 @@ Ext.define('Mba.ux.Environment.overrides.Connection', {
 
     setupUrl: function(options, url)
     {
-        var form = this.getForm(options),
-            regex = null;
+        var form = this.getForm(options);
         if (form) {
             url = url || form.action;
         }
 
         if (!Mba.ux.Environment.config.Url.has(url)) {
-            return url;
+            return this.replaceParamsUrl(options, url);
         }
 
         url = Mba.ux.Environment.config.Url.get(url);
 
+        return this.replaceParamsUrl(options, url);
+    },
+
+    replaceParamsUrl: function(options, url)
+    {
+        var regex;
         if (options.paramsUrl) {
-            for (var i in paramsUrl) {
-                regex = new RegExp('\{' + i + '\}', i);
-                url = url.replace(regex, paramsUrl[i]);
+            for (var i in options.paramsUrl) {
+                regex = new RegExp('\{' + i + '\}', 'i');
+                url = url.replace(regex, options.paramsUrl[i]);
             }
         }
+
         return url;
     }
 });
